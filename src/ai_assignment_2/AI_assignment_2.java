2bc382287d8843b920f1337fc3e1f24550e7e30a
Urvish Rana <urvishrana3728@gmail.com>
2020-03-05 12:54:41 -0600

    code_generation_50%_rb_instance_done

--- src/ai_assignment_2/AI_assignment_2.java
+++ src/ai_assignment_2/AI_assignment_2.java
@@ -5,6 +5,9 @@
  */
 package ai_assignment_2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -14,6 +17,9 @@ import java.util.Scanner;
 public class AI_assignment_2 {
     int numberOfVar,domainSize,noOfConstraint,incompitableTable;
     float p,alpha,r;
+    ArrayList<Integer> variables=new ArrayList <Integer>();
+    ArrayList<Integer> domains=new ArrayList <Integer>();
+    ArrayList<Constraints> constaintVariable;
     /**
      * @param args the command line arguments
      */
@@ -24,7 +30,7 @@ public class AI_assignment_2 {
         
         System.out.println("Enter number of variable (under 1000)");
         ai_assignment.numberOfVar=scanner.nextInt();
-        while (ai_assignment.numberOfVar>1000){
+        while (ai_assignment.numberOfVar>100){
             System.out.println("Pleas enter number of variable (under 1000)");
             ai_assignment.numberOfVar=scanner.nextInt();
         }
@@ -58,6 +64,127 @@ public class AI_assignment_2 {
         System.out.println("Number of Constraint:"+ai_assignment.noOfConstraint);
         ai_assignment.incompitableTable=(int) Math.ceil(ai_assignment.p* ai_assignment.domainSize*ai_assignment.domainSize);
         System.out.println("Number of IncompitableTable size:"+ai_assignment.incompitableTable);
+        
+        //generation of varable
+        System.out.print("Variables:");
+        for(int i=0;i<ai_assignment.numberOfVar;i++){
+            ai_assignment.variables.add(i);
+            System.out.print(" X"+ai_assignment.variables.get(i)+",");
+        }
+        
+        //generation of domain of variables
+        System.out.print("\nDomains:");
+        for(int i=0;i<ai_assignment.domainSize;i++){
+            ai_assignment.domains.add(i);
+            System.out.print(" "+ai_assignment.domains.get(i)+",");
+        }
+        ai_assignment.constaintVariable=new ArrayList<Constraints>();
+        //constraint generation
+        System.out.println("\nConstraints");
+        ai_assignment.generateConstraints();
+        
+        //printing constraints
+        ai_assignment.printConstrains();
+        
+        
+    }
+
+    private void generateConstraints() {
+        Random randomGen=new Random();
+        int i=0,j=0;
+        while (i < noOfConstraint) {
+            int a = randomGen.nextInt(numberOfVar);
+            int b = randomGen.nextInt(numberOfVar);
+            //System.out.println("var a and b" + a + " " + b);
+            if (a != b) {
+                boolean dup = checkforDuplicationForVariable(a, b);
+               // System.out.println("duplication:" + dup);
+                j=0;
+                if (!dup) {
+                    Constraints constraints=new Constraints(a,b);
+                    while (j < incompitableTable) {
+                        int c = randomGen.nextInt(domainSize);
+                        int d = randomGen.nextInt(domainSize);
+                        //System.out.println("values a and b" + a + " " + b);
+                        if (c != d) {
+                            dup = checkforDuplicationForVal(c, d,constraints);
+                            //System.out.println("duplication for val:" + dup);
+                            if(!dup){
+                                constraints.values.add(new Values(c, d));
+                                j++;
+                               //System.out.println(" j:" + j);
+                            }
+                        }
+                    }
+                    constaintVariable.add(constraints);
+                    i++;
+                   // System.out.println("i:" + i);
+                }
+            }
+
+        }
+    }
+
+    private void printConstrains() {
+        for (Constraints c : constaintVariable){
+            System.out.print("\n("+c.var1+","+c.var2+") :");
+            for(Values v: c.values){
+                System.out.print(v.val1+","+v.val2+"\t");
+            }
+        }
+    }
+
+    private boolean checkforDuplicationForVariable(int a, int b) {
+        //System.out.println("checking a and b"+a+ " "+b);
+        //System.out.println("length"+constaintVariable.size());
+        for(Constraints c:constaintVariable){
+            //System.out.println("checking pair:"+c.var1+" "+c.var2);
+            if(c.var1==a && c.var2==b){
+                //System.out.println("true karu 6u");
+                return true;
+            }else if(c.var1==b&&c.var2==a){
+                //System.out.println("2ja ma true karu 6u");
+                return true;
+            }
+               
+        }
+        return false;
+    }
+
+    private boolean checkforDuplicationForVal(int a, int b, Constraints constraints) {
+        //System.out.println("checking a and b"+a+ " "+b);
+        //System.out.println("length"+constaintVariable.size());
+        for(Values c:constraints.values){
+            //System.out.println("checking pair:"+c.val1+" "+c.val2);
+            if(c.val1==a && c.val2==b){
+                //System.out.println("val true karu 6u");
+                return true;
+            }else if(c.val1==b&&c.val2==a){
+                //System.out.println("val 2ja ma true karu 6u");
+                return true;
+            }
+               
+        }
+        return false;
     }
     
+    class Constraints{
+        int var1,var2;
+        ArrayList<Values> values=new ArrayList<Values>();
+
+        private Constraints(int a, int b) {
+            this.var1=a;
+            this.var2=b;
+        }
+        
+    }
+    
+    class Values{
+        int val1,val2;
+
+        private Values(int a, int b) {
+            this.val1=a;
+            this.val2=b;
+        }
+    }
 }
